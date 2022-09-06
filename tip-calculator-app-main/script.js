e940bf2df5a20e8630cb44850adbce114215f465
Reneta <renkagh55@gmail.com>
2022-09-06 12:26:51 +0300

    dom variables

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,17 @@
+"use strict";
+//inputs
+let billInputElement = document.getElementById("bill");
+let numberOfPeopleElement = document.getElementById("bill");
+//div element -- input
+let billInput = document.querySelector(".input-bill");
+let peopleInput = document.querySelector(".input-people");
+//percent buttons
+let fivePercent = document.querySelector(".five");
+let tenPercent = document.querySelector(".ten");
+let fifteenPercent = document.querySelector(".fifteen");
+let twentyfivePercent = document.querySelector(".twentyfive");
+let fiftyPercent = document.querySelector(".fifty");
+// results
+let amoundElement = document.querySelector("#amound-p");
+// reset
+let resetBtnElement = document.querySelector(".btn-reset");
