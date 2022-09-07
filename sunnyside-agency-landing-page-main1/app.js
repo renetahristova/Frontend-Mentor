fbcfd6e970edf9b6343f5172c1005af913e4af5c
Reneta <renkagh55@gmail.com>
2022-09-07 22:33:19 +0300

    hero section

--- sunnyside-agency-landing-page-main1/app.js
+++ sunnyside-agency-landing-page-main1/app.js
@@ -4,5 +4,4 @@ const responsiveElement = document.querySelector(".responsive");
 const hideClassName = "hide";
 menuElement.addEventListener("click", function () {
   responsiveElement.classList.toggle(hideClassName);
-  //   responsiveElement.style.display = "flex";
 });
